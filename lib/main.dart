aff1ef413b9e3c92b8a2d42dbe46592ede635bb9
Lans12345 <olanalans12345@gmail.com>
2022-11-28 12:13:16 +0800

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:consultation_system_mobile/firebase_options.dart';
 import 'package:consultation_system_mobile/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,11 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-     
-     
-      home:  HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
-
-
